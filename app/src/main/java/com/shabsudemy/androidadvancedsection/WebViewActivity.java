2bf63fccbf5bee5de1b7656b0d465661d534fb50
muhammed.shihab <shihabvk99@gmail.com>
2021-05-25 21:11:31 +0530

    added web view and main activity screenshot

--- app/src/main/java/com/shabsudemy/androidadvancedsection/WebViewActivity.java
+++ app/src/main/java/com/shabsudemy/androidadvancedsection/WebViewActivity.java
@@ -3,12 +3,23 @@ package com.shabsudemy.androidadvancedsection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        webView = (WebView) findViewById(R.id.webview);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+
     }
 }
